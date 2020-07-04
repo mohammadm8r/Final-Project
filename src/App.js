bb7055bdb96b5e82df4c4f3b2bf783c9182cffb5
mohammad <rezaeim1376@aut.ac.ir>
2020-07-04 21:17:30 +0430

    AppBar and Drawer Added!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React , { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./Dashboard.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <MenuAppBar />
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Dashboard} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
