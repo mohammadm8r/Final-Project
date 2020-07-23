640afcb09774fc7ea0643398d9559898d9c18000
farziin <farzin.heidary@gmail.com>
2020-07-23 11:25:35 +0430

    99/05/02 11:25 => fixed a bug

--- src/Student/Student Components/Info.js
+++ src/Student/Student Components/Info.js
@@ -10,7 +10,7 @@ import Title from '../../Global Components/Title';
 import { create } from 'jss';
 import rtl from 'jss-rtl';
 import { StylesProvider, jssPreset } from '@material-ui/core/styles';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles'
 import Button from "@material-ui/core/Button";
 import Dialog from '@material-ui/core/Dialog';
@@ -22,48 +22,21 @@ import Demo from '../../Global Components/calendar'
 
 
 const useStyles = theme => ({
-    font: {
-        fontFamily: 'Shabnam',
-      },
+  font: {
+    fontFamily: 'Shabnam',
+  },
 });
 
 class Info extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       nameClass: this.createData.name_of_class,
       rows: [],
       open: false,
       Calendrows: []
     }
-    this.createData = this.createData.bind(this);
-    // this.changeClass = this.changeClass.bind(this);
-    this.handleClickOpen = this.handleClickOpen.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-  }
 
-  createData(id, num_of_absense, num_of_classes, name_of_class, code_of_class) {
-    return { id, num_of_absense, num_of_classes, name_of_class, code_of_class};
-  }
-
-  preventDefault(event) {
-    event.preventDefault();
-  }
-  
-  // changeClass(evenet) {
-  //   console.log(this.state.rows[2])
-  // }
-  handleClickOpen(event) {
-    this.setState({open: true})
-  }
-
-  handleClose(event) {
-    this.setState({open: false})
-  };
-
-  render(){
-    const { classes } = this.props;
-    const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
     this.state.rows.push(
       this.createData(0, '2', '10', 'مبانی برنامه نویسی', '3102110'),
       this.createData(1, '3', '5', 'اندیشه ۱', '1200110'),
@@ -71,6 +44,36 @@ class Info extends React.Component {
       this.createData(4, '2', '10', 'فیزیک ۲', '1203212'),
       this.createData(5, '2', '10', 'فیزیک ۲', '1203212'),
     );
+
+    this.createData = this.createData.bind(this);
+    // this.changeClass = this.changeClass.bind(this);
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  createData(id, num_of_absense, num_of_classes, name_of_class, code_of_class) {
+    return { id, num_of_absense, num_of_classes, name_of_class, code_of_class };
+  }
+
+  preventDefault(event) {
+    event.preventDefault();
+  }
+
+  // changeClass(evenet) {
+  //   console.log(this.state.rows[2])
+  // }
+  handleClickOpen(event) {
+    this.setState({ open: true })
+  }
+
+  handleClose(event) {
+    this.setState({ open: false })
+  };
+
+  render() {
+    const { classes } = this.props;
+    const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
     return (
       <React.Fragment>
         <Title style={{ textAlign: 'center' }}>کلاس‌های ترم جاری</Title>
@@ -88,26 +91,26 @@ class Info extends React.Component {
               <TableRow key={row.id}>
                 <TableCell className={classes.font} style={{ textAlign: 'center' }}>{row.code_of_class}</TableCell>
                 <TableCell className={classes.font} style={{ textAlign: 'center' }}>
-                  <Button style={{cursor:'pointer', fontFamily: 'Shabnam'}} onClick={this.handleClickOpen}>
+                  <Button style={{ cursor: 'pointer', fontFamily: 'Shabnam' }} onClick={this.handleClickOpen}>
                     {row.name_of_class}
                   </Button>
-                    <Dialog
-                        open={this.state.open}
-                        onClose={this.handleClose}
-                        aria-labelledby="alert-dialog-title"
-                        aria-describedby="alert-dialog-description"
-                    >
-                        <DialogContent>
-                            <DialogContentText id="alert-dialog-description" style={{fontFamily: "Shabnam"}}>
-                                <Demo />
-                            </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                        <Button onClick={this.handleClose} color="primary" autoFocus style={{fontFamily: "Shabnam"}}>
-                            بستن
+                  <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                  >
+                    <DialogContent>
+                      <DialogContentText id="alert-dialog-description" style={{ fontFamily: "Shabnam" }}>
+                        <Demo />
+                      </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={this.handleClose} color="primary" autoFocus style={{ fontFamily: "Shabnam" }}>
+                        بستن
                         </Button>
-                        </DialogActions>
-                    </Dialog>
+                    </DialogActions>
+                  </Dialog>
                   {/* <Link style={{textDecoration: 'none', cursor:'pointer'}} to={{pathname:"/MasClasses" , data: this.state.nameClass}}>
                     {row.name_of_class}
                   </Link> */}
